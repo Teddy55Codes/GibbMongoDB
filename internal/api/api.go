0be564b83ed7d351bcc0ed7b082d0f8a6a703396
Fabio Oberhofer <fabio.oberhofer@securiton.ch>
2023-06-21 08:49:52 +0200

    hk formating

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"net/http"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"github.com/Teddy55Codes/GibbMongoDB/internal/store"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-
 type Router struct {
-    database store.Database
+	database store.Database
 }
 
 func Constructor(database store.Database) *Router {
@@ -25,64 +24,64 @@ type entry struct {
 }
 
 func (r *Router) PostEntities(c *gin.Context) {
-		var entry entry
-		if err := c.BindJSON(&entry); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
+	var entry entry
+	if err := c.BindJSON(&entry); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
-		passwordDocument := map[string]interface{}{
-			"1": map[string]string{
-				"name":     entry.Name,
-				"password": entry.Password,
-			},
-		}
+	passwordDocument := map[string]interface{}{
+		"1": map[string]string{
+			"name":     entry.Name,
+			"password": entry.Password,
+		},
+	}
 
-		_, err := r.database.PasswordCollection.InsertOne(context.Background(), passwordDocument)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting into password collection"})
-			return
-		}
+	_, err := r.database.PasswordCollection.InsertOne(context.Background(), passwordDocument)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting into password collection"})
+		return
+	}
 
-		notesDocument := map[string]interface{}{
-			"1": map[string]string{
-				"note": entry.Note,
-			},
-		}
+	notesDocument := map[string]interface{}{
+		"1": map[string]string{
+			"note": entry.Note,
+		},
+	}
 
-		_, err = r.database.NotesCollection.InsertOne(context.Background(), notesDocument)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting into notes collection"})
-			return
-		}
+	_, err = r.database.NotesCollection.InsertOne(context.Background(), notesDocument)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while inserting into notes collection"})
+		return
+	}
 
-		c.JSON(http.StatusOK, gin.H{"status": "Data inserted successfully"})
+	c.JSON(http.StatusOK, gin.H{"status": "Data inserted successfully"})
 }
 
-func (r *Router) GetEntities (c *gin.Context) {
-		passwords, err := r.database.PasswordCollection.Find(context.Background(), bson.M{})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while retrieving from password collection"})
-			return
-		}
-
-		var passwordDocuments []map[string]interface{}
-		if err = passwords.All(context.TODO(), &passwordDocuments); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while decoding password documents"})
-			return
-		}
-
-		notes, err := r.database.NotesCollection.Find(context.Background(), bson.M{})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while retrieving from notes collection"})
-			return
-		}
-
-		var notesDocuments []map[string]interface{}
-		if err = notes.All(context.TODO(), &notesDocuments); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while decoding notes documents"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{"passwords": passwordDocuments, "notes": notesDocuments})
+func (r *Router) GetEntities(c *gin.Context) {
+	passwords, err := r.database.PasswordCollection.Find(context.Background(), bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while retrieving from password collection"})
+		return
 	}
+
+	var passwordDocuments []map[string]interface{}
+	if err = passwords.All(context.TODO(), &passwordDocuments); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while decoding password documents"})
+		return
+	}
+
+	notes, err := r.database.NotesCollection.Find(context.Background(), bson.M{})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while retrieving from notes collection"})
+		return
+	}
+
+	var notesDocuments []map[string]interface{}
+	if err = notes.All(context.TODO(), &notesDocuments); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error while decoding notes documents"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"passwords": passwordDocuments, "notes": notesDocuments})
+}
